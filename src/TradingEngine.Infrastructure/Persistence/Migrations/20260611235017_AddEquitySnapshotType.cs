45dc010fb563b8105ddc10cb1d3d8ffc81971a18
shahin Kiassat <shaahin69@gmail.com>
2026-06-12 00:51:54 +0100

    fix: AddEquitySnapshotType EF migration to resolve PendingModelChangesWarning

---
+++ src/TradingEngine.Infrastructure/Persistence/Migrations/20260611235017_AddEquitySnapshotType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradingEngine.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEquitySnapshotType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "EquitySnapshots",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "EquitySnapshots");
+        }
+    }
+}
