5ec72fec05f77325b10af0f28853d0a3b0b90b94
shahin Kiassat <shaahin69@gmail.com>
2026-06-30 14:05:37 +0100

    chore: untrack wwwroot — Angular build artifacts now gitignored

--- src/TradingEngine.Web/wwwroot/chunk-EODCMHFM.js
+++
@@ -1,1 +0,0 @@
-import{Fa as a,ka as i}from"./chunk-SHTCJODQ.js";var e=class t{saving=i(!1);savedOk=i(!1);static \u0275fac=function(r){return new(r||t)};static \u0275dir=a({type:t})};export{e as a};
